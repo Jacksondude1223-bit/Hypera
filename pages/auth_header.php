9143ac44d63046417814d7d3a304ba05a09cb294
Jacksondude1223-bit <John_Harrison2024@outlook.com>
2025-11-08 16:19:43 -0500

    Update auth_header.php

--- pages/auth_header.php
+++ pages/auth_header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/favicon.png" />
   <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
-  <title>Arclight By Chatnaut Cloud</title>
+  <title>Hypera Virtualization Panel</title>
   <!-- Fonts and icons -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Font Awesome Icons -->
